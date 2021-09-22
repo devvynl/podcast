bd89eb0cb0391da41d47799a91a6fe84b538ac71
Devvyn <devvyn@gmail.com>
2021-09-22 00:55:50 +0000

    jinja implemented and flask working

--- podcast.py
+++ podcast.py
@@ -1,11 +1,16 @@
-from flask import Flask , render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main___':
-    app.run(debug=True, host='0.0.0.0')
 
+
+
+
+if __name__=="__main__":
+    app.run()
